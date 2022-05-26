f95680bd04e9b50ddf6796c70c99337c419038fe
A-cracker <1281590115@qq.com>
2022-05-26 14:59:24 +0800

    初始化项目

---
+++ src/request/api.js
@@ -0,0 +1,3 @@
+import Network from './network';
+//获取个人信息
+export const getInfo = () => Network.get('/test',{});
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>按F12打开控制台查看接口数据</h1>
   </div>
 </template>
+<script>
+import {getInfo} from '@/request/api'
+export default{
+  components: {
+
+  },
+  props: {
+
+  },
+  setup (props) {
+    return {
+      props
+    }
+  },
+  data: () => ({
+  }),
+  computed: {
+  },
+  watch: {
+    
+  },
+  methods: {
+    
+  },
+  mounted(){
+    getInfo().then(res => {
+    console.log(res)
+    });
+  }
+}
+</script>
+
